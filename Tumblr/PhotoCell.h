90837411a83bb90689d0a72c3d474e3352fd5a5c
Stephanie Lampotang <slampotang@fb.com>
2018-06-27 14:27:46 -0700

    may be broken but its all we have

---
+++ Tumblr/PhotoCell.h
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.h
+//  Tumblr
+//
+//  Created by Stephanie Lampotang on 6/27/18.
+//  Copyright © 2018 Stephanie Lampotang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageViewCell;
+
+@end
---
+++ Tumblr/PhotoCell.m
@@ -0,0 +1,26 @@
+//
+//  PhotoCell.m
+//  Tumblr
+//
+//  Created by Stephanie Lampotang on 6/27/18.
+//  Copyright © 2018 Stephanie Lampotang. All rights reserved.
+//
+
+#import "PhotoCell.h"
+
+@implementation PhotoCell
+
+
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
